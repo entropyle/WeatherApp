2d32070d508b797afef83a78939d0ad32982242a
Cody Bellemore <entropyle@gmail.com>
2018-05-04 12:29:58 -0600

    Added function 'getPosition' and 'getWeatherData'

--- app/js/app.js
+++ app/js/app.js
@@ -1,31 +1,32 @@
-let posUrl =
+const posUrl =
   "https://api.ipstack.com/check?access_key=b30492902047f0f621a3df40a4724f2c";
-let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?";
-let weatherUrlApiKey = "b65422c4ce96e72fe468df1c98dc01dc";
 
 let weatherData;
-let latitude;
-let longitude;
 
-fetch(posUrl)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    // latitude = data.latitude;
-    // longitude = data.longitude;
-    weatherUrl = `${weatherUrl}lat=${data.latitude}&lon=${data.longitude}
-      &APPID=${weatherUrlApiKey}`;
-  });
+function getPosition(url) {
+  fetch(url)
+    .then(resp => resp.json())
+    .then(function(data) {
+      let latitude = data.latitude;
+      let longitude = data.longitude;
+      let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?";
+      let weatherUrlApiKey = "b65422c4ce96e72fe468df1c98dc01dc";
+      return `${weatherUrl}lat=${data.latitude}&lon=
+        ${data.longitude}&APPID=${weatherUrlApiKey}`;
+    });
+}
 
-fetch(weatherUrl)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    weatherData = data;
-    console.log(weatherData);
-  });
+function getWeatherData(url) {
+  fetch(url)
+    .then(resp => resp.json())
+    .then(function(data) {
+      let weatherData = {
+        // location: data.;
+        // temperature: data.;
+      };
+      return weatherData;
+    });
+}
 
 // const weatherInfo = document.getElementById("weather-info");
 // const location = document.getElementById("location");

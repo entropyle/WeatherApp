622f8fb2b508cc02bc9de42757b84bf973bc29df
Cody Bellemore <entropyle@gmail.com>
2018-05-05 19:13:13 -0600

    Rework api functions

--- app/js/app.js
+++ app/js/app.js
@@ -1,64 +1,40 @@
+const weatherInfo = document.getElementById("weather-info");
+const loc = document.getElementById("location");
+const temperature = document.getElementById("temperature");
+const weather = document.getElementById("weather");
+
 const posUrl =
-  "https://api.ipstack.com/check?access_key=b30492902047f0f621a3df40a4724f2c";
+  "http://api.ipstack.com/check?access_key=b30492902047f0f621a3df40a4724f2c";
 
-let weatherData;
-
-function getPosition(url) {
+let getPosition = function(url) {
   fetch(url)
     .then(resp => resp.json())
     .then(function(data) {
-      let latitude = data.latitude;
-      let longitude = data.longitude;
-      let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?";
-      let weatherUrlApiKey = "b65422c4ce96e72fe468df1c98dc01dc";
-      return `${weatherUrl}lat=${data.latitude}&lon=
-        ${data.longitude}&APPID=${weatherUrlApiKey}`;
+      const wurl = "https://api.openweathermap.org/data/2.5/weather?";
+      const wurlApiKey = "b65422c4ce96e72fe468df1c98dc01dc";
+      let locData = {
+        city: data.city,
+        country: data.country_code,
+        longitude: data.longitude,
+        latitude: data.latitude,
+        weatherUrl: `${wurl}lat=${data.latitude}&lon=${
+          data.longitude
+        }&APPID=${wurlApiKey}`
+      };
+      return getWeatherData(locData.weatherUrl);
     });
-}
+};
 
-function getWeatherData(url) {
+let getWeatherData = function(url) {
   fetch(url)
     .then(resp => resp.json())
     .then(function(data) {
       let weatherData = {
-        // location: data.;
-        // temperature: data.;
+        weather: data.weather[0].main,
+        temperature: data.main.temp
       };
       return weatherData;
     });
-}
+};
 
-// const weatherInfo = document.getElementById("weather-info");
-// const location = document.getElementById("location");
-// const temperature = document.getElementById("temperature");
-// const weather = document.getElementById("weather");
-
-// let weatherUrl = "https://fcc-weather-api.glitch.me/api/current?";
-// var pos = [];
-
-// function getLocation() {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition((pos = getPosition()));
-//     weatherData = getWeatherData();
-//   } else {
-//     weatherInfo.innerHTML = "Geolocation is not supported by this browser.";
-//   }
-// }
-
-// function getPosition(position) {
-//   let pos = [];
-//   pos[0] = position.coords.latitute;
-//   pos[1] = position.coords.longitude;
-//   return pos;
-//   //weatherUrl = `${weatherUrl}long=${lon}&lat=${lat}`;
-// }
-
-// function getWeatherData() {
-//   fetch(weatherUrl)
-//     .then(resp => resp.json())
-//     .then(function(data) {
-//       return data;
-//     });
-// }
-
-// getLocation();
+console.log(getPosition(posUrl));

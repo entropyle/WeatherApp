56963a71ff998769666e083b257504c62f519729
entropyle <entropyle@gmail.com>
2018-05-05 21:47:29 -0600

    Update api calls

--- app/js/app.js
+++ app/js/app.js
@@ -6,35 +6,27 @@ const weather = document.getElementById("weather");
 const posUrl =
   "http://api.ipstack.com/check?access_key=b30492902047f0f621a3df40a4724f2c";
 
-let getPosition = function(url) {
-  fetch(url)
-    .then(resp => resp.json())
-    .then(function(data) {
-      const wurl = "https://api.openweathermap.org/data/2.5/weather?";
-      const wurlApiKey = "b65422c4ce96e72fe468df1c98dc01dc";
-      let locData = {
-        city: data.city,
-        country: data.country_code,
-        longitude: data.longitude,
-        latitude: data.latitude,
-        weatherUrl: `${wurl}lat=${data.latitude}&lon=${
-          data.longitude
-        }&APPID=${wurlApiKey}`
-      };
-      return getWeatherData(locData.weatherUrl);
-    });
+let getWeatherData = async function(url) {
+  let resp = await fetch(url);
+  let data = await resp.json();
+  const wurl = "https://api.openweathermap.org/data/2.5/weather?";
+  const wurlApiKey = "b65422c4ce96e72fe468df1c98dc01dc";
+  let locData = {
+    city: data.city,
+    country: data.country_code,
+    longitude: data.longitude,
+    latitude: data.latitude,
+    weatherUrl: `${wurl}lat=${data.latitude}&lon=${
+      data.longitude
+    }&APPID=${wurlApiKey}`
+  };
+  resp = await fetch(locData.weatherUrl);
+  data = await resp.json();
+  let weatherData = {
+    weather: data.weather[0].main,
+    temperature: data.main.temp
+  };
+  console.log(locData.city, locData.country, weatherData);
 };
 
-let getWeatherData = function(url) {
-  fetch(url)
-    .then(resp => resp.json())
-    .then(function(data) {
-      let weatherData = {
-        weather: data.weather[0].main,
-        temperature: data.main.temp
-      };
-      return weatherData;
-    });
-};
-
-console.log(getPosition(posUrl));
+getWeatherData(posUrl);
